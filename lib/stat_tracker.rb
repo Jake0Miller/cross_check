97a1e439b30b1a6ef75a999449d9d52dd56577e3
Jake Miller <jake0miller@gmail.com>
2019-05-20 15:50:54 -0600

    Refactor variable names in from_csv

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -8,21 +8,21 @@ class StatTracker
   end
 
   def self.from_csv(locations)
-    games = get_games(locations[:games])
-    teams = get_teams(locations[:teams])
-    game_teams = get_game_teams(locations[:game_teams])
-    StatTracker.new(games, teams, game_teams)
+    games_path = get_games(locations[:games])
+    teams_path = get_teams(locations[:teams])
+    game_teams_path = get_game_teams(locations[:game_teams])
+    StatTracker.new(games_path, teams_path, game_teams_path)
   end
 
-  def self.get_games(games)
-
-  end
-
-  def self.get_teams(teams)
+  def self.get_games(games_path)
 
   end
 
-  def self.get_game_teams(game_teams)
-    
+  def self.get_teams(teams_path)
+
+  end
+
+  def self.get_game_teams(game_teams_path)
+
   end
 end
