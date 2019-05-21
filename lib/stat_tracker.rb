ae3de9f51fdf7deeb4e90a48174cf43c66fa8b40
Tay <tayd93@hotmail.com>
2019-05-21 15:30:49 -0600

    Cleans up syntax in stat_tracker class/test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,4 @@
 require 'csv'
-require 'pry'
 
 class StatTracker
   attr_reader :games, :teams, :game_teams
@@ -13,24 +12,15 @@ class StatTracker
   def self.from_csv(locations)
     games = get_games(locations[:games])
     teams = get_teams(locations[:teams])
-    game_teams_path = get_game_teams(locations[:game_teams])
-    StatTracker.new(games, teams, game_teams_path)
-  end
-
-  def self.get_games(games_path)
-
+    game_teams = get_game_teams(locations[:game_teams])
+    StatTracker.new(games, teams, game_teams)
   end
 
   def self.get_teams(teams_path) #team instance
-    rows = CSV.read('data/team_info.csv', headers: true)
     teams = {}
-    rows.each do |row|
+    CSV.foreach('data/team_info.csv', headers: true) do |row|
       teams[row[0]] = Team.new(row)
     end
     teams
-  end
-
-  def self.get_game_teams(game_teams_path)
-
   end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,8 +1,4 @@
 require './test/test_helper'
-require './lib/stat_tracker'
-require './lib/team'
-require 'pry'
-
 
 class StatTrackerTest < Minitest::Test
   def setup
@@ -19,24 +15,9 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of StatTracker, @stat_tracker
   end
 
-  def test_get_games
-    skip
-    expected = ["2012030221","20122013","P",2013-05-16,"3","6",2,3,
-          "home win OT","left","TD Garden","/api/v1/venues/null",
-          "America/New_York",-4,"EDT"]
-    assert_equal expected, @stat_tracker.games[0]
-  end
-
   def test_get_teams
     @get_teams = StatTracker.get_teams(@team_path)
     assert_equal 33, @get_teams.size
-  end
-
-  def test_get_game_teams
-    skip
-    expected = ["2012030221","3","away",FALSE,"OT","John Tortorella",2,35,44,8,
-            3,0,44.8,17,7]
-    assert_equal expected, @stat_tracker.game_teams[0]
   end
 
   def test_from_csv
