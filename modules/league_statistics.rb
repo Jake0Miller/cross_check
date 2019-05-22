9488a1b9aa2c5e1bf5896c0fb4ca03417e83fca7
Jake Miller <jake0miller@gmail.com>
2019-05-22 13:39:58 -0600

    Refactor leagut statistics to dry up code

--- modules/league_statistics.rb
+++ modules/league_statistics.rb
@@ -3,32 +3,43 @@ module LeagueStatistics
     @teams.size
   end
 
-  def total_goals
+  def total_goals(home=true, away=true)
     @games.inject({}) do |hash, game|
-      hash[game.away_team_id.to_sym] ||= 0
-      hash[game.away_team_id.to_sym] += game.away_goals.to_i
-      hash[game.home_team_id.to_sym] ||= 0
-      hash[game.home_team_id.to_sym] = game.home_goals.to_i
+      if away
+        hash[game.away_team_id.to_sym] ||= 0
+        hash[game.away_team_id.to_sym] += game.away_goals.to_i
+      end
+      if home
+        hash[game.home_team_id.to_sym] ||= 0
+        hash[game.home_team_id.to_sym] = game.home_goals.to_i
+      end
       hash
     end
   end
 
-  def total_games
+  def total_games(home=true, away=true)
     @games.inject({}) do |hash, game|
-      hash[game.away_team_id.to_sym] ||= 0
-      hash[game.away_team_id.to_sym] += 1
-      hash[game.home_team_id.to_sym] ||= 0
-      hash[game.home_team_id.to_sym] = 1
+      if away
+        hash[game.away_team_id.to_sym] ||= 0
+        hash[game.away_team_id.to_sym] += 1
+      end
+      if home
+        hash[game.home_team_id.to_sym] ||= 0
+        hash[game.home_team_id.to_sym] = 1
+      end
       hash
     end
   end
 
-  def average_score
+  def average_score(home = true, away = true)
     @teams.inject({}) do |hash, team|
       if total_goals[team[0]].nil?
         hash
       else
-        hash[team[0]] = total_goals[team[0]] / total_games[team[0]].to_f
+        goals = total_goals(home, away)[team[0]]
+        games = total_games(home, away)[team[0]]
+        hash[team[0]] = goals / games.to_f
+        # hash[team[0]] = total_goals[team[0]] / total_games[team[0]].to_f
       end
       hash
     end
@@ -71,74 +82,20 @@ module LeagueStatistics
     @teams[average_goals_allowed.max_by { |k,v| v }.first].team_name
   end
 
-  def total_goals_away
-    @games.inject({}) do |hash, game|
-      hash[game.away_team_id.to_sym] ||= 0
-      hash[game.away_team_id.to_sym] += game.away_goals.to_i
-      hash
-    end
-  end
-
-  def total_goals_home
-    @games.inject({}) do |hash, game|
-      hash[game.home_team_id.to_sym] ||= 0
-      hash[game.home_team_id.to_sym] = game.home_goals.to_i
-      hash
-    end
-  end
-
-  def total_away_games
-    @games.inject({}) do |hash, game|
-      hash[game.away_team_id.to_sym] ||= 0
-      hash[game.away_team_id.to_sym] += 1
-      hash
-    end
-  end
-
-  def total_home_games
-    @games.inject({}) do |hash, game|
-      hash[game.home_team_id.to_sym] ||= 0
-      hash[game.home_team_id.to_sym] = 1
-      hash
-    end
-  end
-
-  def average_score_away
-    @teams.inject({}) do |hash, team|
-      if total_goals[team[0]].nil?
-        hash
-      else
-        hash[team[0]] = total_goals_away[team[0]] / total_games[team[0]].to_f
-      end
-      hash
-    end
-  end
-
-  def average_score_home
-    @teams.inject({}) do |hash, team|
-      if total_goals[team[0]].nil?
-        hash
-      else
-        hash[team[0]] = total_goals_home[team[0]] / total_games[team[0]].to_f
-      end
-      hash
-    end
-  end
-
   def highest_scoring_visitor
-    @teams[average_score_away.max_by { |k,v| v }.first].team_name
+    @teams[average_score(home = false).max_by { |k,v| v }.first].team_name
   end
 
   def lowest_scoring_visitor
-    @teams[average_score_away.min_by { |k,v| v }.first].team_name
+    @teams[average_score(home = false).min_by { |k,v| v }.first].team_name
   end
 
   def highest_scoring_home_team
-    @teams[average_score_home.max_by { |k,v| v }.first].team_name
+    @teams[average_score(away = false).max_by { |k,v| v }.first].team_name
   end
 
   def lowest_scoring_home_team
-    @teams[average_score_home.min_by { |k,v| v }.first].team_name
+    @teams[average_score(away = false).min_by { |k,v| v }.first].team_name
   end
 
   def total_wins
